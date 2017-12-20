781d96bde5b6310892297a5efcbae9661cb693fa
jeremiehvt <jeremiehvt@gmail.com>
2017-12-20 10:48:24 +0100

    suppression de la page contact et du module visuel de connection pour refonte

--- view/frontend/header.php
+++ view/frontend/header.php
@@ -14,15 +14,10 @@
         <ul class="nav navbar-nav">
           <li class="active"><a href="index.php">Accueil</a></li>
           <li><a href="index.php?page=allposts">Billet simple pour l'Alaska</a></li>
-          <li><a href="index.php?page=contact">Contact</a></li>
+          
         </ul>
 
-        <form action="index.php?page=login" method="POST" class="navbar-form navbar-right inline-form">
-          <input type="text" name="pseudo" id="pseudo" class="input-sm form-control" placeholder="pseudo"> 
-          <input type="password" name="password" id="password" class="input-sm form-control" placeholder="mot de passe">
-          <button class="btn btn-primary btn-sm">log-in</button>
-          <a href="index.php?page=inscription" class="btn btn-default btn-sm">sign-in</a>
-        </form>
+        
     </div>
 
   </nav>
